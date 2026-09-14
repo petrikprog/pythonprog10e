6ad0efc3606cf6b09a3a14de608368e97a2a799d
Kovács Máté Attila <kovacs.mate.attila@petrik.hu>
2026-09-14 13:13:26 +0200

    creation

---
+++ 260907/elso.py
@@ -0,0 +1,17 @@
+print("Helló 10E!(World)")
+#end: a sor végi karakter, alapértelmezetten SorvégeKocsivissza (Enter)\n
+print("Szia Én!", end=" *** ")
+print("Programozni királyságos", end=" *** ")
+print("Üdv a programomban!")
+print()
+#komment - megjegyzés a kódben-egy soros: #
+"""print()
+    Ez egy több soros komment-több soros \"""
+"""
+
+#példa a sep használatára: paraméterek elválasztása
+print("Tej", "Sört NEM", "Kakaó", "Kenyér", "Macisajt", "Koffeinmentes KV", sep="\t")
+#\t = tabulátor
+print("Tej", "Sört NEM", "Kakaó", "Kenyér", "Macisajt", "Koffeinmentes KV", sep="; ")
+#szintaktikai hiba a nyelvhelyességével kapcsolatos hiba
+#szemantikai hiba a logikával kapcsolatos hiba
---
+++ 260907/feladat.py
@@ -0,0 +1,37 @@
+# 1. feladat
+print("Hello World")
+
+# 2. feladat
+print("Kedves Máté!")
+print("        Isten hozott a Python világában!")
+print("                Üdv: a program")
+print("Kedves Máté!\n\tIsten hozott a Python világában!\n\t\tÜdv: a program")
+"""
+Kedves Máté!
+    Isten hozott a Python világában!
+        Üdv: a program
+"""
+
+
+# 3. feladat
+print("Ha meg tudod álmodni, meg is tudod tenni!", end=" ")
+print("Walt Disney")
+print("Ha meg tudod álmodni, meg is tudod tenni!", end="-")
+print("Walt Disney")
+print("Walt Disney:", end="\t")
+print("Ha meg tudod álmodni, meg is tudod tenni!")
+
+# 4. feladat
+print("tej", "kenyér", "sajt", "vaj", sep=",")
+print("tej", "kenyér", "sajt", "vaj", sep="\t")
+print("tej", "kenyér", "sajt", "vaj", sep="***")
+
+# 5. feladat
+print("\"A 'whitespace' vagy 'white space' angol szóösszetétel, jelentése fehér tér.\n\n" \
+"Az informatikában, elsősorban a\n\t" \
+"szövegszerkesztésben és\n\t" \
+"programozásban\n" \
+"használatos kifejezés.\n\n" \
+"Alapvetően azokat a karaktereket értjük alatta, amelyek nem láthatóak a szövegben,\n" \
+"viszont valamilyen egyedi funkcióval bírnak. Nincs elterjedt magyar kifejezés rá.\"\n\t" \
+"--A wikipédiából,(a szabad enciklopédiából)")
